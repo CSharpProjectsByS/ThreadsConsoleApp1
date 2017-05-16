dac28b51c38bb429c1c78720e78d93506e1e0f9e
Dariusz Nurzyński <Dariusz Nurzyski>
2017-05-16 13:51:50 +0200

    Wersja bez sortowań

--- ThreadsConsoleApp1/Services/ThreadsService.cs
+++ ThreadsConsoleApp1/Services/ThreadsService.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Windows.Threading;
+
 
 namespace ThreadsConsoleApp1.Services
 {
     class ThreadsService
     {
+        private int lastPrimeThread1 = 0;
+        private int lastPrimeThread2 = 0;
+        private int lastPrimeThread3 = 0;
+        private int lastPrimeThread4 = 0;
+        private int lastPrimeThread5 = 0;
+
+        private int progress1 = 0;
+        private int progress2 = 0;
+        private int progress3 = 0;
+        private int progress4 = 0;
+        private int progress5 = 0;
+
+
+        private readonly object locker1 = new object();
+        private readonly object locker2 = new object();
+        private readonly object locker3 = new object();
+        private readonly object locker4 = new object();
+        private readonly object locker5 = new object();
+
+        private int numberOfAttempts = 1000000;
+
+        System.Timers.Timer timer;
+
         public ThreadsService()
         {
             RunScenerio();
@@ -17,20 +37,20 @@ namespace ThreadsConsoleApp1.Services
 
         public void RunScenerio()
         {
-            int n = 1000000;
-            Thread thread1 = new Thread(() => DoSomething(n, "Watek1"));
+            
+            Thread thread1 = new Thread(() => DoSomething(numberOfAttempts, ref lastPrimeThread1, ref progress1, locker1));
             thread1.Priority = ThreadPriority.Highest;
 
-            Thread thread2 = new Thread(() => DoSomething(n, "Watek2"));
+            Thread thread2 = new Thread(() => DoSomething(numberOfAttempts, ref lastPrimeThread2, ref progress2, locker2));
             thread2.Priority = ThreadPriority.AboveNormal;
 
-            Thread thread3= new Thread(() => DoSomething(n, "Watek3"));
+            Thread thread3= new Thread(() => DoSomething(numberOfAttempts, ref lastPrimeThread3, ref progress3, locker3));
             thread3.Priority = ThreadPriority.Normal;
 
-            Thread thread4 = new Thread(() => DoSomething(n, "Watek4"));
+            Thread thread4 = new Thread(() => DoSomething(numberOfAttempts, ref lastPrimeThread4, ref progress4,  locker4));
             thread4.Priority = ThreadPriority.BelowNormal;
 
-            Thread thread5 = new Thread(() => DoSomething(n, "Watek5"));
+            Thread thread5 = new Thread(() => DoSomething(numberOfAttempts, ref lastPrimeThread5, ref progress5, locker5));
             thread5.Priority = ThreadPriority.Lowest;
 
             thread1.Start();
@@ -39,32 +59,69 @@ namespace ThreadsConsoleApp1.Services
             thread4.Start();
             thread5.Start();
 
-        }
+            timer = new System.Timers.Timer();
 
-        private void DoSomething(int n, String name)
+            timer.Interval = 2000;
+            timer.Elapsed += showResult;
+
+            timer.Start();
+
+        } 
+
+        private void DoSomething(int n, ref int lastPrime,ref int progress, object locker)
         {
-            Stopwatch sw = new Stopwatch();
-            int lastPrime = 0;
-
-            sw.Start();
+            Console.WriteLine("Jestem w wątku");
             for (int i = 0; i <= n; i++)
             {
                 if (isPrimeNumber(i))
                 {
-                    lastPrime = i;
+                    lock (locker)
+                    {
+                        lastPrime = i;
+                    }
+                    
                 }
-
-                TimeSpan time = sw.Elapsed;
-
-                if (time.Seconds % 2 == 0 && time.Seconds >= 2)
+                lock (locker)
                 {
-                    Console.WriteLine(name + ": " + lastPrime);
-                }
-                
-                
-            
+                    progress = i;
+                } 
             }
-            sw.Stop();
+            progress++;
+        }
+
+        private void showResult(object sender, object e)
+        {
+            Console.WriteLine("Jestem w show Result");
+            if (!isDoneWithPrimes())
+            {
+                Console.WriteLine("Wyniki");
+                lock (locker1)
+                {
+                    Console.WriteLine("Highest: {0}", lastPrimeThread1);
+                }
+                lock (locker2)
+                {
+                    Console.WriteLine("AboveNormal: {0}", lastPrimeThread2);
+                }
+                lock (locker3)
+                {
+                    Console.WriteLine("Normal: {0}", lastPrimeThread3);
+                }
+                lock (locker4)
+                {
+                    Console.WriteLine("BelowNormal: {0}", lastPrimeThread4);
+                }
+                lock (locker5)
+                {
+                    Console.WriteLine("Lowest: {0}", lastPrimeThread5);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Zadanie zakończone.");
+                timer.Stop();
+            }
         }
 
         private bool isPrimeNumber(int number)
@@ -76,6 +133,49 @@ namespace ThreadsConsoleApp1.Services
                     return false;
                 }
             }
+
+            return true;
+        }
+
+        private bool isDoneWithPrimes()
+        {
+            Console.WriteLine("Jestem w isDoneWIthPrimes");
+            lock (locker1)
+            {
+                if (progress1 < numberOfAttempts)
+                {
+                    return false;
+                }
+            }
+            lock (locker2)
+            {
+                if (progress2 < numberOfAttempts)
+                {
+                    return false;
+                }
+            }
+            lock (locker3)
+            {
+                if (progress3 < numberOfAttempts)
+                {
+                    return false;
+                }
+            }
+            lock (locker4)
+            {
+                if (progress4 < numberOfAttempts)
+                {
+                    return false;
+                }
+            }
+            lock (locker5)
+            {
+                if (progress5 < numberOfAttempts)
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine("To jeszcze nie konieć z primamai");
 
             return true;
         }
